ee336f01d615f70c9b6b4e9105ddec0737aad7fb
clebercristiam <cleberctr@gmail.com>
2022-03-26 17:56:03 -0300

    casa
    mudanças 1

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -13,10 +13,21 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W));
+        if (Input.GetKeyUp(KeyCode.W))
         {
-            
+            Debug.Log("Botão W foi pressionado");
         }
-
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Botão S foi pressionado");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Botão D foi pressionado");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Botão A foi pressionado");
+        }
     }
 }
